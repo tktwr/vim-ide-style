6162f06a67029b82052d900b879a99f118e1793b
tktwr <takehiro.tawara@gmail.com>
2022-12-18 01:34:35 +0900

    move vimapi.sh to function for speed up

--- bin/vimapi.sh
+++ bin/vimapi.sh
@@ -1,62 +1,10 @@
 #!/bin/bash
 
-bin_name=vimapi.sh
-
-g_tapi="Tapi_Exec"
-g_args=""
-
-f_help() {
-  echo "NAME"
-  echo "  $bin_name"
-  echo
-  echo "SYNOPSIS"
-  echo "  $bin_name [options]"
-  echo
-  echo "OPTIONS"
-  echo "  -h, --help     ... print help"
-  echo "  --in-prev-win  ... exec in prev win"
-  echo "  --in-above-win ... exec in above win"
-  echo "  --in-new-tab   ... exec in new tab"
-}
-
 f_vimapi() {
-  printf '\e]51;["call","%s","%s"]\x07' "$g_tapi" "$1" 1>&2
+  local tapi=$1
+  shift
+  printf '\e]51;["call","%s","%s"]\x07' "$tapi" "$*" 1>&2
 }
 
-f_parse_args() {
-  while [ $# -gt 0 ]; do
-    case "$1" in
-      -h|--help)
-        f_help
-        exit
-        ;;
-      --in-prev-win)
-        g_tapi="Tapi_ExecInPrevWin"
-        ;;
-      --in-above-win)
-        g_tapi="Tapi_ExecInAboveWin"
-        ;;
-      --in-new-tab)
-        g_tapi="Tapi_ExecInNewTab"
-        ;;
-      --exec-echo)
-        g_tapi="Tapi_ExecEcho"
-        ;;
-      *)
-        g_args="$*"
-        break
-        ;;
-    esac
-    shift
-  done
-}
-
-f_print_args() {
-  echo "g_tapi = $g_tapi" 1>&2
-  echo "g_args = $g_args" 1>&2
-}
-
-f_parse_args "$@"
-#f_print_args
-f_vimapi "$g_args"
+f_vimapi "$@"
 
--- bin/vimdirdiff.sh
+++ bin/vimdirdiff.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$VIM_TERMINAL" ]; then
-  vimapi.sh "call VisTabDirDiff('$1', '$2')"
+  vimapi_exec "call VisTabDirDiff('$1', '$2')"
 else
   vim -c "call VisTabDirDiff('$1', '$2')"
 fi
